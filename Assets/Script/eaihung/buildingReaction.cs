7a70bef6ed6c9586327d300b1b3e52daf435afbd
EaiHung <yihungchou.cs04g@g2.nctu.edu.tw>
2017-01-21 19:35:43 +0800

    the newest

--- Assets/Script/eaihung/buildingReaction.cs
+++ Assets/Script/eaihung/buildingReaction.cs
@@ -6,21 +6,37 @@ public class buildingReaction : MonoBehaviour {
     public float hp ;
     public List<GameObject> locationStr;
     public float power = 1;
+
+    public Vector3 originalPos;
 	// Use this for initialization
 	void Start () {
         hp = 100;
+        originalPos = transform.position;
 	}
-    void causeDmg() {
-        float[] str = new float[4];
-        str[0] = 0; //abs(0-8)
-        str[1] = 0; //abs(3-5)
-        str[2] = 0; //abs(2-6)
-        str[3] = 0; //abs(1-7)
 
-        hp -= ( str[0] + str[1] + str[2] + str[3] ) * Time.deltaTime * power;
+    void causeDmg() {
+        float[] str = new float[12];
+
+
+        float w_strength=0;
+        for (int i = 0; i < 12; i++)
+        {
+            str[i] = Mathf.Abs(locationStr[i].transform.position.y - locationStr[24 - i].transform.position.y)*0.1f;
+            w_strength += str[i];
+        }
+
+        transform.position = originalPos +new Vector3(Random.Range(-w_strength, w_strength), Random.Range(-w_strength, w_strength), Random.Range(-w_strength, w_strength));
+            //print("yo");
+            //GetComponent<Rigidbody>().AddForce(Random.Range(-5, 5f), Random.Range(-5f, 5f), Random.Range(-5f, 5f)*100);
+
+        //    hp -= (str[0] + str[1] + str[2] + str[3]) * Time.deltaTime * power;
     }
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            causeDmg();
+        }
         causeDmg();
 	}
 }
