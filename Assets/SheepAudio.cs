ef64788a535017c53a53d46374ba4843f1585184
Yang <blue574961@gmail.com>
2017-01-22 03:05:18 +0800

    123

---
+++ Assets/SheepAudio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SheepAudio : MonoBehaviour {
+
+    private AudioSource source;
+	// Use this for initialization
+	void Start ()
+    {
+        source = GetComponent<AudioSource>();
+        source.clip = Resources.Load<AudioClip>("Sounds/sheep_me");
+    }
+    void OnCollisionExit(Collision other)
+    {
+        if (other.collider.tag == "Floor" && !source.isPlaying)
+        {
+            source.Play();
+        }
+    }
+}
