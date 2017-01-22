8db71e6770430ce2521bd7ec2090945c0e548f98
Yang <blue574961@gmail.com>
2017-01-22 10:07:02 +0800

    結束報紙

--- Assets/Script/GameUI.cs
+++ Assets/Script/GameUI.cs
@@ -5,18 +5,38 @@ using UnityEngine.UI;
 
 public class GameUI : MonoBehaviour {
     [SerializeField]
-    private Sprite[] newspaper = new Sprite[2];
-    // Use this for initialization
+    private Sprite[] _newspapers = new Sprite[2];
+    private Image newspaper;
+    private Animator anim_newspaper;
+    public enum Result { Success , Lose };
+    
     void Start ()
     {
+        newspaper = GameObject.Find("newspaper").GetComponent<Image>();
+        newspaper.color = new Color(1, 1, 1, 0);
+        newspaper.enabled = false;
+        anim_newspaper = newspaper.GetComponent<Animator>();
+    }
+    public void IsGameOver(Result result)
+    {
         
+        switch (result)
+        {
+            case Result.Success:
+                newspaper.sprite = _newspapers[1];
+                break;
+            case Result.Lose:
+                newspaper.sprite = _newspapers[0];
+                break;
+            default:
+                break;
+        }
+        newspaper.enabled = true;
+        anim_newspaper.SetBool("IsEnding", true);
+        StartCoroutine(DisplayImage(newspaper));
+
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
     IEnumerator DisplayImage(Image ig)
     {
         while (ig.color.a < 1)
