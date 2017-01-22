1e80f64fb8c07784caad60f5d666d82e71524720
EaiHung <yihungchou.cs04g@g2.nctu.edu.tw>
2017-01-22 10:12:52 +0800

    test

---
+++ Assets/Script/UI_button_eqp/wqpButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wqpButton : MonoBehaviour {
+    public int level;
+
+
+    public void setLevel() {
+        GameObject.Find("GameSystem").GetComponent<waveGenerator>().level = level;
+        GameObject.Find("Happy").SetActive(false);
+        GameObject.Find("Crazy").SetActive(false);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
