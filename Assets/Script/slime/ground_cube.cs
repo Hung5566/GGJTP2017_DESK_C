918b89ed510cec79554bb6a44d38338f10dd494f
SlimeMeteor <slime.meteor@gamenation.com.tw>
2017-01-22 13:55:48 +0800

    Add ground wave prototype version 2.

--- Assets/Script/slime/ground_cube.cs
+++ Assets/Script/slime/ground_cube.cs
@@ -5,21 +5,27 @@ public class ground_cube : MonoBehaviour
 {
 	public Vector3_Int cube_process_data;
 
-	public bool act;
-	public int delay;
-	public int power;
+	// debug info.
+//	public bool ok;
+//	public bool act;
+//	public int delay;
+//	public int power;
+//	public Vector3 new_pos;
 
 	void Update ()
 	{
 		cube_process_data.Tick();
 
-		act = cube_process_data.is_active;
-		delay = cube_process_data.delay;
-		power = cube_process_data.power;
+		// debug info.
+//		ok = cube_process_data.is_enable;
+//		act = cube_process_data.is_active;
+//		delay = cube_process_data.delay;
+//		power = cube_process_data.power;
+//		new_pos = cube_process_data.GetNewPoint();
 
 		transform.position = cube_process_data.GetNewPoint();
 
-		if (cube_process_data.IsActive() == false) {
+		if (cube_process_data.IsEnable() == false) {
 			GetComponent<Renderer>().material.color = Color.white;
 			Destroy(this);
 		}
